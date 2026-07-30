65491234fa592401fb0120f23647ff002bf375e6
mauricionlz2018-tech <mauricionlz2018@gmail.com>
2026-07-30 13:39:54 -0600

    Actualizar

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME,       // abarrotes_db
   process.env.DB_USER,       // app_user
-  process.env.DB_PASSWORD,   // AppUserPass456
+  process.env.DB_PASSWORD,   
   {
     host: process.env.DB_HOST,      // host interno de Railway
     port: process.env.DB_PORT,      // 3306
